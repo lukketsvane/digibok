c306653db8923bbf85ab0578d7f608269c886913
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-01-31 02:06:41 +0000

    Update Streamlit configurations and fix deprecated option

--- setup_streamlit.py
+++ setup_streamlit.py
@@ -1,4 +1,8 @@
 import warnings
 warnings.filterwarnings('ignore')
-import streamlit.cli
-streamlit.cli._main()
+import streamlit as st
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('global.developmentMode', False)
+st.script_run_ctx.add_script_run_ctx()
+st._is_running_with_streamlit = True
+import app
